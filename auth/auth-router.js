4e29da56e56e2462005aad8300edaeb0e2de8e10
Alex Karavil <46430499+Karavil@users.noreply.github.com>
2020-04-24 11:35:42 -0400

    Completed password hashing + storing it in database for backend authentication router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,62 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.post('/register', (req, res) => {
-  // implement registration
+const { PrismaClient } = require("@prisma/client");
+const db = new PrismaClient();
+
+const bcrypt = require("bcryptjs");
+
+router.post("/register", async (req, res) => {
+   const { username, password } = req.body;
+   if (username && password) {
+      const saltRounds = process.env.BCRYPT_SALT_ROUNDS || 12;
+      const hashedPass = bcrypt.hashSync(password, saltRounds);
+
+      const newUser = await db.users.create({
+         data: {
+            username,
+            password: hashedPass,
+         },
+      });
+
+      res.status(200).json({
+         message: `User with id ${newUser.id} created!`,
+      });
+   } else {
+      res.status(400).json({
+         message:
+            "Please include a username and password in your request body.",
+      });
+   }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", async (req, res) => {
+   const { username, password } = req.body;
+
+   if (username && password) {
+      const newUser = await db.users.findOne({
+         where: {
+            username,
+         },
+      });
+
+      if (newUser === null) {
+         return res
+            .status(404)
+            .json({ message: `User (${username}) not found.` });
+      }
+
+      if (bcrypt.compareSync(password, newUser.password) === true) {
+         return res.status(200).json({
+            message: `Password approved, logging you in.`,
+         });
+      }
+
+      return res.status(400).json({ message: "Invalid password." });
+   }
+
+   return res.status(400).json({
+      message: "Please include a username and password in your request body.",
+   });
 });
 
 module.exports = router;
